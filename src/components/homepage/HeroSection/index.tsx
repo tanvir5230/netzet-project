9b11bbdd2dc7da462e69da6e2fee6962867c4f84
tanvir5230 <tanvirbintouhid5230@gmail.com>
2025-08-01 21:04:06 +0600

    fix: hero image will be centered

--- src/components/homepage/HeroSection/index.tsx
+++ src/components/homepage/HeroSection/index.tsx
@@ -15,11 +15,11 @@ const HeroSection: React.FC<React.HTMLAttributes<HTMLDivElement>> = ({
       )}
       {...props}
     >
-      <HeroImage className="w-full -translate-y-1 lg:hidden" />
+      <HeroImage className="mx-auto -translate-y-1 lg:hidden" />
       <HeroContent className="w-full lg:w-6/12 z-10" />
       <div></div>
       <div className="hidden absolute -top-10 -right-[140px] bottom-0 w-[666px] lg:flex items-center justify-center">
-        <HeroImage className="w-full h-full max-h-[679px]" />
+        <HeroImage className="w-full max-h-[679px]" />
       </div>
     </div>
   );
