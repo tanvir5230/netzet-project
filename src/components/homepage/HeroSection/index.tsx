3ed3585bd1521a9d6a39eedd5c50ec811cb382d2
tanvir5230 <tanvirbintouhid5230@gmail.com>
2025-08-01 20:12:47 +0600

    refactor: The Hero image rendering process in made better

--- src/components/homepage/HeroSection/index.tsx
+++ src/components/homepage/HeroSection/index.tsx
@@ -15,10 +15,12 @@ const HeroSection: React.FC<React.HTMLAttributes<HTMLDivElement>> = ({
       )}
       {...props}
     >
-      <HeroImage className="w-full lg:hidden" />
+      <HeroImage className="w-full -translate-y-3 lg:hidden" />
       <HeroContent className="w-full lg:w-6/12 z-10" />
       <div></div>
-      <div className="hidden lg:block absolute -top-10 -right-[140px] bottom-0 w-[666px] min-h-full bg-[url('/images/hero-image.png')] bg-no-repeat"></div>
+      <div className="hidden absolute -top-10 -right-[140px] bottom-0 w-[666px] lg:flex items-center justify-center">
+        <HeroImage className="w-full h-full max-h-[679px]" />
+      </div>
     </div>
   );
 };
